2f3667e974ebc4c72f0b962db3cdb2b6aa73a56f
Furqon-M <furchon.muddafar@gmail.com>
2024-07-30 11:17:09 +0700

    if else di pyhton

---
+++ main.py
@@ -0,0 +1,32 @@
+#   If dan Else Statements
+
+# 1. if nya
+# 2. kondisinya
+# 3. aksinya
+
+nama = input("Siapa nama anda?")
+
+# program sebelumnya
+# if kondisi: aksi
+# program selanjutnya
+
+# 1. program if inline
+# if nama=="kelana" : print("Kamu Tampan Sekali")
+# print(f"Terima kasih {nama}")
+
+# 2. program if indentation
+
+# if nama=="kelana":
+#    print("Kamu Tampan Sekali")
+#    print("Kamu juga keren banget")
+# print(f"Terima kasih {nama}")
+
+# print("======================")
+
+# 3. Else Statements
+
+if nama=="kelana":
+    print("Kamu Tampan Sekali")
+else:
+    print("Kamu Kurang Tampan")
+print(f"Terima kasih {nama}")
